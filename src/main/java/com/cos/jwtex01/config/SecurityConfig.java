3fd321c0f4a12db1aa5feae4c0db9e939906b9b9
jg20228 <jg20228@nate.com>
2020-08-07 11:50:10 +0900

    SecurityConfig 설정

---
+++ src/main/java/com/cos/jwtex01/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.cos.jwtex01.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity //시큐리티 활성화 -> 기본 스프링 필터체인에 등록
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http //stateful 사용 안할려고 설정
+				.csrf().disable()//csrf는 필요가 없음, 이게 있으면 postman 작동이 안함
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//세션 사용X
+			.and()
+				.formLogin().disable() //form 로긴 막음
+				.httpBasic().disable() //http Jsession방식 사용안함
+				//필터 추가
+				.addFilter(null) //내가 만든 인증 필터 
+				.addFilter(null)
+				.authorizeRequests()//모든 권한 요청에 대해서
+				//antMatchers를 걸고 네거티브 방식 사용
+				.antMatchers("/api/v1/manager/**")
+					.access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+				.antMatchers("/api/v1/admin/**")
+					.access("hasRole('ROLE_ADMIN')")
+				.anyRequest().permitAll();
+				//.authenticated()을 걸면 위 페이지 외에 모두 인증이 필요함
+	}
+}
--- src/main/java/com/cos/jwtex01/controller/RestApiController.java
+++ src/main/java/com/cos/jwtex01/controller/RestApiController.java
@@ -1,10 +1,7 @@
 package com.cos.jwtex01.controller;
 
-import static org.hamcrest.CoreMatchers.nullValue;
-
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
