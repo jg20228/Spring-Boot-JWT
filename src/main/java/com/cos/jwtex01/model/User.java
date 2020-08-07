8ddc3fde156e643bb190004387e0143a9be19ee0
jg20228 <jg20228@nate.com>
2020-08-07 12:08:12 +0900

    헷갈리지 않게 permission이랑 Active 삭제

--- src/main/java/com/cos/jwtex01/model/User.java
+++ src/main/java/com/cos/jwtex01/model/User.java
@@ -19,18 +19,12 @@ public class User {
     @Column(nullable = false)
     private String password;
 
-    private int active;
-
     private String roles = "";
 
-    private String permissions = "";
-
-    public User(String username, String password, String roles, String permissions){
+    public User(String username, String password, String roles){
         this.username = username;
         this.password = password;
         this.roles = roles;
-        this.permissions = permissions;
-        this.active = 1;
     }
 
     protected User(){}
@@ -47,29 +41,14 @@ public class User {
         return password;
     }
 
-    public int getActive() {
-        return active;
-    }
-
     public String getRoles() {
         return roles;
-    }
-
-    public String getPermissions() {
-        return permissions;
     }
 
     //ENUM으로 안하고 ,로 해서 구분해서 ROLE을 입력 -> 그걸 파싱
     public List<String> getRoleList(){
         if(this.roles.length() > 0){
             return Arrays.asList(this.roles.split(","));
-        }
-        return new ArrayList<>();
-    }
-
-    public List<String> getPermissionList(){
-        if(this.permissions.length() > 0){
-            return Arrays.asList(this.permissions.split(","));
         }
         return new ArrayList<>();
     }
